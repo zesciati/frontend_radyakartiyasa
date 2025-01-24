41167b4bb16a436ae285439e636e647d7b0596c6
zesciati <zakyabiyyu2003@gmail.com>
2025-01-24 14:30:31 +0800

    menghapus file toml

--- frontend-radyakartiyasa_astro/digital-doppler/astro.config.mjs
+++ frontend-radyakartiyasa_astro/digital-doppler/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  root: './src/pages', // Arahkan root ke folder `pages`
-  outDir: '../dist',   // Output hasil build, sesuaikan dengan keperluan
+  // root: './src/pages', // Arahkan root ke folder `pages`
+  // outDir: '../dist',   // Output hasil build, sesuaikan dengan keperluan
 });
---
+++ frontend-radyakartiyasa_astro/dist/_astro/Hero.astro_astro_type_script_index_0_lang.F0B5ns68.js
@@ -0,0 +1,1 @@
+import"https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js";

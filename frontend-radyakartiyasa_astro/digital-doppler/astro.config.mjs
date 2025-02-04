7bbfab18d44bae5c2bdad70562feb4c464ae3055
zesciati <zakyabiyyu2003@gmail.com>
2025-02-04 13:02:56 +0800

    menghapus node_modules

--- frontend-radyakartiyasa_astro/digital-doppler/astro.config.mjs
+++ frontend-radyakartiyasa_astro/digital-doppler/astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
+  adapter: node({
+    mode: 'standalone',
   }),
 });
